e0ac2d9dfba4f63bacae02d2ffd130781b9b4336
David A. Divas <62566465+Daldiv@users.noreply.github.com>
2022-03-08 17:12:27 -0600

    added alpaca changes

--- hummingbot/connector/exchange/alpaca/alpaca_constants.py
+++ hummingbot/connector/exchange/alpaca/alpaca_constants.py
@@ -1,78 +1,56 @@
 # A single source of truth for constant variables related to the exchange
 
 from hummingbot.core.api_throttler.data_types import RateLimit
-#REQUIRED
+
 EXCHANGE_NAME = "alpaca"
-PAPER_URL ="https://paper-api.alpaca.markets/"
-PAPER_KEY ="PKH3PPQCW6GLS8CY166G"
-PAPER_SECRET = "F8adr3BjoEzEkfOKCoO3XxaW1KYtgpSkXgL6OpDx"
-
-REST_URL = "https://broker-api.alpaca.markets/"
-REST_URL_SANDBOX = "https://broker-api.sandbox.alpaca.markets/"
-WSS_URL = "wss://stream.data.alpaca.markets/v2/sip"
-WSS_URL_SANDBOX = "wss://stream.data.alpaca.markets/v2/iex"
-
-#HBOT  Stuff
-HBOT_ORDER_ID_PREFIX = "x-XEKWYICX"
-
-#todo; figure out how to handle dynamic order_id variables or appened in request.
+REST_URL = "https://paper-api.alpaca.markets"
+REST_URL_HK = "https://data.alpaca.markets/v2"
+WSS_URL = "wss://stream.data.alpaca.markets/v2/iex"
+WSS_URL_HK = "wss://stream.data.alpaca.markets/v2/iex"
 
 # REST API ENDPOINTS
-CHECK_NETWORK_PATH_URL = "system/service"
-GET_ASSET_PATH_URL = "/v2/assets"
-GET_ASSET_SYMBOL_PATH_URL = "/v2/assets/:symbol"
-GET_TRADES_STATUS_PATH_URL = "/v1/events/trades"
-GET_ORDER_BOOK_PATH_URL = "/v2/orders"
-CREATE_ORDER_PATH_URL = "/v2/orders"
-DELETE_ORDER_PATH_URL = "/v2/orders/{order_id}"
-GET_ORDER_DETAIL_PATH_URL = "/v2/orders/{order_id}"
-PATCH_ORDER_PATH_URL = "/v2/orders/{order_id}"
-#MISC FUNCTIONS
-GET_ACCOUNT_SUMMARY_PATH_URL = "/v2/account/"
-GET_OPEN_ORDERS_PATH_URL = "/v2/orders"
-DELETE_ALL_ORDERS_PATH_URL = "/v2/orders/"
-#POSITIONS
-GET_ALL_POSITIONS_PATH_URL = "/v2/positions"
-DELETE_ALL_POSITIONS_PATH_URL = "/v2/positions"
-DELETE_SPECIFIC_POSITION_PATH_URL = "/v2/positions/{symbol}"
-#PORTFOLIO HISTORY
-GET_PORTFOLIO_HISTORY_PATH_URL = "/v2/account/portfolio/history"
+CHECK_NETWORK_PATH_URL = "v2/clock"
+GET_TRADING_PAIRS_PATH_URL = "v2/assets"
+GET_LAST_TRADING_PRICES_PATH_URL = "spot/v1/ticker"
+GET_ORDER_BOOK_PATH_URL = "spot/v1/symbols/book"
+CREATE_ORDER_PATH_URL = "v2/orders"
+CANCEL_ORDER_PATH_URL = "v2/orders"
+GET_ACCOUNT_SUMMARY_PATH_URL = "v2/account"
+GET_ORDER_DETAIL_PATH_URL = "v2/orders"
+GET_TRADE_DETAIL_PATH_URL = "spot/v1/trades"
+GET_OPEN_ORDERS_PATH_URL = "v2/positions"
+
 
 # WS API ENDPOINTS
 WS_CONNECT = "WSConnect"
 WS_SUBSCRIBE = "WSSubscribe"
 
-# BitMart has a per method API limit
+# Alpaca has a per method API limit
 RATE_LIMITS = [
-    RateLimit(limit_id=CHECK_NETWORK_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_ASSET_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_ASSET_SYMBOL_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_TRADES_STATUS_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_ORDER_BOOK_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=CREATE_ORDER_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=DELETE_ORDER_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_ACCOUNT_SUMMARY_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_ORDER_DETAIL_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=PATCH_ORDER_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_OPEN_ORDERS_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=DELETE_ALL_ORDERS_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_ALL_POSITIONS_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=DELETE_ALL_POSITIONS_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=DELETE_SPECIFIC_POSITION_PATH_URL, limit=15, time_interval=1),
-    RateLimit(limit_id=GET_PORTFOLIO_HISTORY_PATH_URL, limit=15, time_interval=1),
-
+    RateLimit(limit_id=CHECK_NETWORK_PATH_URL, limit=10, time_interval=1),
+    RateLimit(limit_id=GET_TRADING_PAIRS_PATH_URL, limit=5, time_interval=1),
+    RateLimit(limit_id=GET_TRADING_RULES_PATH_URL, limit=5, time_interval=1),
+    RateLimit(limit_id=GET_LAST_TRADING_PRICES_PATH_URL, limit=5, time_interval=1),
+    RateLimit(limit_id=GET_ORDER_BOOK_PATH_URL, limit=5, time_interval=1),
+    RateLimit(limit_id=CREATE_ORDER_PATH_URL, limit=50, time_interval=1),
+    RateLimit(limit_id=CANCEL_ORDER_PATH_URL, limit=50, time_interval=1),
+    RateLimit(limit_id=GET_ACCOUNT_SUMMARY_PATH_URL, limit=10, time_interval=1),
+    RateLimit(limit_id=GET_ORDER_DETAIL_PATH_URL, limit=50, time_interval=1),
+    RateLimit(limit_id=GET_TRADE_DETAIL_PATH_URL, limit=10, time_interval=1),
+    RateLimit(limit_id=GET_OPEN_ORDERS_PATH_URL, limit=10, time_interval=1),
     RateLimit(limit_id=WS_CONNECT, limit=1, time_interval=1),
     RateLimit(limit_id=WS_SUBSCRIBE, limit=60, time_interval=600),
 ]
 
 ORDER_STATUS = {
-    1: "new",               # The order has been received by Alpaca
-    2: "partially_filled",  # Partially filled
-    3: "filled",            # Completed
-    4: "done_for_day",      # The order is done executing for the day,
-    5: "canceled",          # The order has been canceled, and no further updates will occur for the order.
-    6: "expired",           # The order has expired, and no further updates will occur for the order.
-    7: "replaced",          # The order was replaced by another order, or was updated due to a market event.
-    8: "pending_cancel",    # The order is waiting to be canceled.
-    9: "pending_replace",   # The order is waiting to be replaced by another order.
+    1: "FAILED",        # Order failure
+    2: "OPEN",          # Placing order
+    3: "REJECTED",      # Order failure, Freeze failure
+    4: "ACTIVE",        # Order success, Pending for fulfilment
+    5: "ACTIVE",        # Partially filled
+    6: "FILLED",        # Fully filled
+    7: "ACTIVE",        # Canceling
+    8: "CANCELED",      # Canceled
+    9: "ACTIVE",        # Outstanding (4 and 5)
+    10: "COMPLETED"     # 6 and 8
 }
--- hummingbot/connector/exchange/alpaca/alpaca_utils.py
+++ hummingbot/connector/exchange/alpaca/alpaca_utils.py
@@ -1,80 +1,176 @@
-import os
-import socket
-from typing import Any, Dict
-
-import hummingbot.connector.exchange.alpaca.alpaca_constants as CONSTANTS
+import math
+import zlib
+from typing import Dict, List, Tuple
 
 from hummingbot.client.config.config_methods import using_exchange
 from hummingbot.client.config.config_var import ConfigVar
-from hummingbot.core.utils.tracking_nonce import get_tracking_nonce
+from hummingbot.core.data_type.order_book_message import OrderBookMessage
+from hummingbot.core.data_type.order_book_row import OrderBookRow
+from hummingbot.core.utils.tracking_nonce import get_tracking_nonce, get_tracking_nonce_low_res
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
+CENTRALIZED = True
 
+EXAMPLE_PAIR = "ETH-USDT"
+
+DEFAULT_FEES = [0.25, 0.25]
+
+HBOT_BROKER_ID = "hummingbot-"
+
+
+# deeply merge two dictionaries
+def merge_dicts(source: Dict, destination: Dict) -> Dict:
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge_dicts(value, node)
+        else:
+            destination[key] = value
+
+    return destination
+
+
+# join paths
+def join_paths(*paths: List[str]) -> str:
+    return "/".join(paths)
+
+
+# get timestamp in milliseconds
+def get_ms_timestamp() -> int:
+    return get_tracking_nonce_low_res()
+
+
+# convert milliseconds timestamp to seconds
+def ms_timestamp_to_s(ms: int) -> int:
+    return math.floor(ms / 1e3)
+
+
+def convert_snapshot_message_to_order_book_row(message: OrderBookMessage) -> Tuple[List[OrderBookRow], List[OrderBookRow]]:
+    update_id = message.update_id
+    data = message.content
+    bids, asks = [], []
+
+    if "buys" in data:
+        bids = [
+            OrderBookRow(float(bid["price"]), float(bid["amount"]), update_id) for bid in data["buys"]
+        ]
+    elif "bids" in data:
+        bids = [
+            OrderBookRow(float(bid[0]), float(bid[1]), update_id) for bid in data["bids"]
+        ]
+    sorted(bids, key=lambda a: a.price)
+
+    if "sells" in data:
+        asks = [
+            OrderBookRow(float(ask["price"]), float(ask["amount"]), update_id) for ask in data["sells"]
+        ]
+    elif "asks" in data:
+        asks = [
+            OrderBookRow(float(ask[0]), float(ask[1]), update_id) for ask in data["asks"]
+        ]
+    sorted(asks, key=lambda a: a.price)
+
+    return bids, asks
+
+
+def convert_diff_message_to_order_book_row(message: OrderBookMessage) -> Tuple[List[OrderBookRow], List[OrderBookRow]]:
+    update_id = message.update_id
+    data = message.content
+    bids, asks = [], []
+
+    if "buys" in data:
+        bids = [
+            OrderBookRow(float(bid["price"]), float(bid["amount"]), update_id) for bid in data["buys"]
+        ]
+    elif "bids" in data:
+        bids = [
+            OrderBookRow(float(bid[0]), float(bid[1]), update_id) for bid in data["bids"]
+        ]
+    sorted(bids, key=lambda a: a.price)
+
+    if "sells" in data:
+        asks = [
+            OrderBookRow(float(ask["price"]), float(ask["amount"]), update_id) for ask in data["sells"]
+        ]
+    elif "asks" in data:
+        asks = [
+            OrderBookRow(float(ask[0]), float(ask[1]), update_id) for ask in data["asks"]
+        ]
+    sorted(asks, key=lambda a: a.price)
+
+    return bids, asks
+
+
+# Request ID class
+class RequestId:
+    """
+    Generate request ids
+    """
+    _request_id: int = 0
+
+    @classmethod
+    def generate_request_id(cls) -> int:
+        return get_tracking_nonce()
+
+
+def convert_from_exchange_trading_pair(exchange_trading_pair: str) -> str:
+    return exchange_trading_pair.replace("_", "-")
+
+
+def convert_to_exchange_trading_pair(hb_trading_pair: str) -> str:
+    return hb_trading_pair.replace("-", "_")
 
 
 def get_new_client_order_id(is_buy: bool, trading_pair: str) -> str:
-    """
-    Creates a client order id for a new order
-    :param is_buy: True if the order is a buy order, False otherwise
-    :param trading_pair: the trading pair the order will be operating with
-    :return: an identifier for the new order to be used in the client
-    """
-    #todo; fix code below for Alpaca API
     side = "B" if is_buy else "S"
-    symbols = trading_pair.split("-")
-    base = symbols[0].upper()
-    quote = symbols[1].upper()
-    base_str = f"{base[0]}{base[-1]}"
-    quote_str = f"{quote[0]}{quote[-1]}"
-    client_instance_id = hex(abs(hash(f"{socket.gethostname()}{os.getpid()}")))[2:6]
-    return f"{CONSTANTS.HBOT_ORDER_ID_PREFIX}-{side}{base_str}{quote_str}{client_instance_id}{get_tracking_nonce()}"
+    return f"{HBOT_BROKER_ID}{side}-{trading_pair}-{get_tracking_nonce()}"
 
 
-def is_exchange_information_valid(exchange_info: Dict[str, Any]) -> bool:
-    """
-    Verifies if a trading pair is enabled to operate with based on its exchange information
-    :param exchange_info: the exchange information for a trading pair
-    :return: True if the trading pair is enabled, False otherwise
-    """
-    return exchange_info.get("status", None) == "TRADING" and "SPOT" in exchange_info.get("permissions", list())
+# Decompress WebSocket messages
+def decompress_ws_message(message):
+    if type(message) == bytes:
+        decompress = zlib.decompressobj(-zlib.MAX_WBITS)
+        inflated = decompress.decompress(message)
+        inflated += decompress.flush()
+        return inflated.decode('UTF-8')
+    else:
+        return message
 
 
-def public_rest_url(path_url: str, domain: str = "com") -> str:
-    """
-    Creates a full URL for provided public REST endpoint
-    :param path_url: a public REST endpoint
-    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
-    :return: the full URL to the endpoint
-    """
-    return CONSTANTS.REST_URL.format(domain) + path_url
-
-
-def private_rest_url(path_url: str, domain: str = "com") -> str:
-    """
-    Creates a full URL for provided private REST endpoint
-    :param path_url: a private REST endpoint
-    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
-    :return: the full URL to the endpoint
-    """
-    return CONSTANTS.REST_URL.format(domain) + path_url
-
-
-def build_api_factory() -> WebAssistantsFactory:
-    api_factory = WebAssistantsFactory()
-    return api_factory
+def compress_ws_message(message):
+    if type(message) == str:
+        message = message.encode()
+        compress = zlib.compressobj(wbits=-zlib.MAX_WBITS)
+        deflated = compress.compress(message)
+        deflated += compress.flush()
+        return deflated
+    else:
+        return message
 
 
 KEYS = {
     "alpaca_api_key":
         ConfigVar(key="alpaca_api_key",
-                  prompt="Enter your Alpaca API key >>> ",
+                  prompt="Enter your Alpaca Trading API key >>> ",
                   required_if=using_exchange("alpaca"),
                   is_secure=True,
                   is_connect_key=True),
-    "alpaca_api_secret":
-        ConfigVar(key="alpaca_api_secret",
-                  prompt="Enter your Alpaca API secret >>> ",
+    "alpaca_secret_key":
+        ConfigVar(key="alpaca_secret_key",
+                  prompt="Enter your Alpaca secret key >>> ",
+                  required_if=using_exchange("alpaca"),
+                  is_secure=True,
+                  is_connect_key=True),
+    "alpaca_memo":
+        ConfigVar(key="alpaca_memo",
+                  prompt="Enter your Alpaca API Memo >>> ",
                   required_if=using_exchange("alpaca"),
                   is_secure=True,
                   is_connect_key=True),
 }
+
+
+def build_api_factory() -> WebAssistantsFactory:
+    api_factory = WebAssistantsFactory()
+    return api_factory
